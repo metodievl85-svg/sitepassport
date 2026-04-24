afe08b1a18c1c377f254f7eb5c34a0ff986bf9c1
Lyuben Metodiev <info@sitepassportapp.co.uk>
2026-04-24 19:40:20 +0100

    Add SitePassport favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,11 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'SitePassport',
   description: 'Operative passport dashboard',
+  icons: {
+    icon: '/sitepassport-icon.svg',
+    shortcut: '/sitepassport-icon.svg',
+    apple: '/sitepassport-icon.svg',
+  },
 }
 
 export default function RootLayout({
