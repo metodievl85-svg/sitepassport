3f2bdf60123029256e8195c9b44f5c3a9ab0f259
Lyuben Metodiev <info@sitepassportapp.co.uk>
2026-06-09 17:50:42 +0100

    Widen agency dashboard menu button to prevent text wrap

--- app/agency/page.tsx
+++ app/agency/page.tsx
@@ -561,7 +561,7 @@ export default function AgencyPage() {
               </h1>
               <p style={{ margin: 0 }}>{email}</p>
             </div>
-            <div style={{ flexShrink: 0, width: '160px' }}>
+            <div style={{ flexShrink: 0, width: '190px' }}>
               <AgencyDashboardMenu onMessages={handleScrollToMessages} onSignOut={handleLogout} onScanQR={() => { router.push('/scan') }} />
             </div>
           </div>
